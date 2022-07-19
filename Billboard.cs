481ae13d1c2ee0ab0624fe3a4b060b447f598780
ybin <80818871+jungyoubin@users.noreply.github.com>
2022-07-19 11:29:12 +0900

    Add files via upload

---
+++ Billboard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Camera camTr;
+    //private Transform tr;
+    //private float posX;
+    //private float posZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTr = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = camTr.transform.position - transform.position;
+        v.x = v.z = 0;
+        transform.LookAt(camTr.transform.position - v);
+        Debug.Log(camTr.transform.position - v);
+
+    }
+}
